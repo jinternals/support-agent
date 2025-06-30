507d9f935aecc7a43b366a35644bd00352bcd0e7
Mradul Pandey <pandeymradul@gmail.com>
2025-06-30 14:43:24 +0530

    Updated code

--- support-agent-app/src/main/java/com/jinternals/support/agent/controllers/AssistantController.java
+++ support-agent-app/src/main/java/com/jinternals/support/agent/controllers/AssistantController.java
@@ -3,12 +3,8 @@ package com.jinternals.support.agent.controllers;
 import com.jinternals.support.agent.domain.Answer;
 import com.jinternals.support.agent.domain.Question;
 import com.jinternals.support.agent.services.AssistantService;
-import io.modelcontextprotocol.spec.McpSchema;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
